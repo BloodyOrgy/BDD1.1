aa85bf2dc67a9e08da0331c2f827ce1847a4c749
Satana <rad_devil@inbox.ru>
2025-02-10 21:02:01 +0400

    createDataHelper

--- src/test/java/data/DataHelper.java
+++ src/test/java/data/DataHelper.java
@@ -1,5 +1,32 @@
 package data;
 
 import lombok.Value;
+
 public class DataHelper {
+    private DataHelper(){
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo ("vasya","qwerty123");
+    }
+
+    public static AuthInfo getOtherAuthInfo(AuthInfo original) {
+        return new AuthInfo ("vasya","123qwerty");
+    }
+
+
+    public static VerificationCode getVerificationCodeFor(AuthInfo original) {
+        return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
 }
